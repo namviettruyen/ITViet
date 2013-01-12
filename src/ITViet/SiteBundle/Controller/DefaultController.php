684a0b9969144a8ce9c8fcdf28875f32e57b3cd9
Nam <nam.luu@gmail.com>
2013-01-12 19:38:42 +0700

    remove nonsense

--- src/ITViet/SiteBundle/Controller/DefaultController.php
+++ src/ITViet/SiteBundle/Controller/DefaultController.php
@@ -8,8 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('ITVietSiteBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('ITVietSiteBundle:Default:index.html.twig', array('name' => 'HelloWorld'));
     }
 }
