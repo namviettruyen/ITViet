bf751c8cca842412c14fc2930e305d9a274f6ee2
Nam <nam.luu@gmail.com>
2013-02-23 16:10:16 +0700

    fix bug index page has problem with the pagination

--- src/ITViet/SiteBundle/Controller/TopController.php
+++ src/ITViet/SiteBundle/Controller/TopController.php
@@ -11,14 +11,28 @@ class TopController extends Controller
     /**
      * @Template()
      */
-    public function indexAction($page)
+    public function indexAction()
     {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $articleRepos = $em->getRepository('ITVietSiteBundle:Article');
+        $articles_per_page = $this->container->getParameter('max_articles_on_toppage');
+
+        $articles = $articleRepos->getArticlesActive($articles_per_page, 0);
+        return array(
+          'articles' => $articles,
+        );
+    }
+
+    /**
+     * @Template()
+     */
+    public function newestAction($page) {
         $em = $this->get('doctrine.orm.entity_manager');
         $articleRepos = $em->getRepository('ITVietSiteBundle:Article');
         $articles_per_page = $this->container->getParameter('max_articles_on_toppage');
         $display = 5;
         $count = $articleRepos->getCount();
-        $paginator = new Paginator('_homepage', array(), $count, $display, $articles_per_page, $page);
+        $paginator = new Paginator('_articles_newest', array(), $count, $display, $articles_per_page, $page);
 
         $articles = $articleRepos->getArticlesActive($paginator->pageSize, $paginator->offset);
         return array(
